a8e7e77015d63126f190491d30d2434dca13cf74
williamGOC <oropesaw1993@gmail.com>
2025-11-14 14:05:26 -0300

    --

--- move.c
+++ move.c
@@ -644,7 +644,7 @@ int main(int argc, char **argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
     glutInitWindowSize(windowWidth, windowHeight);
-    glutCreateWindow("Particle Simulation - SIS Epidemic Model");
+    glutCreateWindow("SIS Epidemic Model");
 
     initOpenGL();
 
